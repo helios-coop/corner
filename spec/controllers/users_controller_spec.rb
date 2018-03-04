5c5210e2a775efd290e2676451d4ffaf531721d7
David Ladowitz <david@ladowitz.com>
2018-03-04 15:00:09 -0800

    Add signup flow (#13)
    * Add signup flow
    
    * fix rubocop warnings
    
    * fix rubocop warnings
    
    * fix spec

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+  describe 'GET #new' do
+    before { get :new }
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'finds a new user object' do
+      expect(assigns(:user)).to be_a User
+    end
+  end
+
+  describe 'POST #create' do
+    context 'when password confirmation matches' do
+      let(:user_params) { {display_name: 'AAntonop', email: 'andrea@mastering_bitcoin.com', password: 'permissionless', password_confirmation: 'permissionless'} }
+
+      it 'creates a new user account' do
+        post :create, params: {user: user_params}
+        expect(assigns(:user).persisted?).to be true
+      end
+    end
+
+    context 'when password confirmation does not match' do
+      let(:user_params) { {display_name: 'AAntonop', email: 'andrea@mastering_bitcoin.com', password: 'permissionless', password_confirmation: 'consenys'} }
+
+      it 'does not create a new user account' do
+        post :create, params: {user: user_params}
+        expect(assigns(:user).persisted?).to be false
+      end
+    end
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,5 +4,7 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of(:display_name) }
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:password_digest) }
+  it { should validate_uniqueness_of(:display_name) }
+  it { should validate_uniqueness_of(:email) }
   it { should have_many(:submissions) }
 end
