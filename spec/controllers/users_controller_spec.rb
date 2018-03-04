3c1d450ab1cbfb14d803972280c2187a4feef46c
David Ladowitz <david@ladowitz.com>
2018-03-04 14:46:51 -0800

    fix rubocop warnings

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,9 +2,7 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
   describe 'GET #new' do
-    subject { get :new }
-
-    before { subject }
+    before { get :new }
 
     it 'returns http success' do
       expect(response).to have_http_status(:success)
@@ -16,20 +14,17 @@ RSpec.describe UsersController, type: :controller do
   end
 
   describe 'POST #create' do
-    before { subject }
-
     context 'when password confirmation matches' do
-      subject { post :create, params: {user: {display_name: 'AAntonopoulos', email: 'andrea@mastering_bitcoin.com', password: 'permissionless', password_confirmation: 'permissionless'}} }
-
       it 'creates a new user account' do
+        let(:user_params) { {display_name: 'AAntonop', email: 'andrea@mastering_bitcoin.com', password: 'permissionless', password_confirmation: 'permissionless'} }
+        post :create, params: {user: user_params}
         expect(assigns(:user).persisted?).to be true
       end
     end
 
     context 'when password confirmation does not match' do
-      subject { post :create, params: {user: {display_name: 'AAntonopoulos', email: 'andrea@mastering_bitcoin.com', password: 'permissionless', password_confirmation: 'consensys'}} }
-
       it 'does not create a new user account' do
+        let(:user_params) { {display_name: 'AAntonop', email: 'andrea@mastering_bitcoin.com', password: 'permissionless', password_confirmation: 'consenys'} }
         expect(assigns(:user).persisted?).to be false
       end
     end
