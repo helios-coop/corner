39ea72dc58a160c9e5d426cd8893ebb02b9e2abd
David Ladowitz <david@ladowitz.com>
2018-03-12 20:59:11 -0700

    Allow show anonymously

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ListingsController < ApplicationController
-  before_action :authorize, except: [:index]
+  before_action :authorize, except: %i[index show]
 
   def index
     @listings = Listing.order(:name)
