db0cd8ef4348219f85882ce13a698fa4f0631504
David Ladowitz <david@ladowitz.com>
2018-03-17 19:06:47 -0700

    Save thumbnail url when creating listing (#53)

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,6 +2,7 @@
 
 class Listing < ApplicationRecord
   validates :name, :submitter_id, presence: true
+  validates :google_places_id, uniqueness: true
 
   has_many :currencies_listings
   has_many :currencies, through: :currencies_listings
