a7e29866fd7d4db6ccb12759c258d83cd2200d75
David Ladowitz <david@ladowitz.com>
2018-04-07 14:17:28 -0700

    Dladowitz/mobile optimize attempt (#101)
    * detecting mobile
    
    * create mobile layouts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
 
+  before_action :detect_device_variant
+
   private
 
   def current_user
@@ -22,5 +24,12 @@ class ApplicationController < ActionController::Base
 
     flash[:danger] = "You must be and Admin to do that."
     redirect_to root_path
+  end
+
+  # http://blog.chrismaxwell.com/mobile-device-detection-with-ruby-on-rails
+  def detect_device_variant
+    return unless browser.device.mobile?
+    request.variant = :phone
+    prepend_view_path Rails.root + "app" + "views" + "mobile_views"
   end
 end
