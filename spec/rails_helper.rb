8ee6180229757f904e4edde284ecf12dac24bc9d
Robert Fletcher <lobatifricha@gmail.com>
2018-04-07 17:58:55 -0700

    Refactors: use Rails.root.join

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,7 +30,7 @@ require "support/auth_helpers"
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+# Dir[Rails.root.join("spec", "support", "**", "*.rb")].each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
@@ -41,7 +41,7 @@ RSpec.configure do |config|
   config.include(FactoryBot::Syntax::Methods)
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.fixture_path = Rails.root.join("spec", "fixtures")
   config.global_fixtures = :all
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
