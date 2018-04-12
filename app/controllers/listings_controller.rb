d9fd6d215b53764ba9e3cc1f212e79d69071f3af
Robert Fletcher <lobatifricha@gmail.com>
2018-04-11 23:05:44 -0700

    Rubocop: pull place params parsing out into poro
    This creates a new ruby object to encapsulate the params parsing from
    the google places api.

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -18,7 +18,7 @@ class ListingsController < ApplicationController
 
   def create
     listing_attributes = listing_attributes_from_params
-    @listing = Listing.new(listing_attributes)
+    @listing = Listing.new(listing_attributes.merge(submitter: current_user))
 
     if @listing.save
       @listing.currencies = Currency.where(id: params[:currencies])
@@ -85,47 +85,8 @@ class ListingsController < ApplicationController
       .merge(submitter_id: current_user.id)
   end
 
-  ADDR_COMPONENTS_MAP = {
-    street_number: :street_number,
-    route: :route,
-    locality: :city,
-    administrative_area_level_1: :state,
-    country: :country,
-    postal_code: :zipcode,
-  }.freeze
-
   def listing_attributes_from_params
-    g_place = JSON.parse(params["google-place"]).deep_symbolize_keys
-
-    phone = g_place[:formatted_phone_number]
-    phone = phone.gsub(/[()+\s-]/, "") if phone
-
-    attrs = {
-      name: g_place[:name],
-      phone: phone,
-      url: g_place[:website],
-      lat: g_place[:geometry][:location][:lat],
-      long: g_place[:geometry][:location][:lng],
-      thumbnail_url: g_place[:thumbnailUrl],
-      submitter_id: current_user.id,
-      google_places_id: g_place[:place_id],
-    }
-
-    g_place[:address_components].each do |component|
-      component_type = component[:types][0].to_sym
-      address_component = ADDR_COMPONENTS_MAP[component_type]
-
-      next unless address_component
-
-      attrs[address_component] =
-        if address_component == :city
-          component[:long_name]
-        else
-          component[:short_name]
-        end
-    end
-    attrs[:address] = "#{attrs.delete(:street_number)} #{attrs.delete(:route)}"
-    attrs
+    GooglePlaceParamsParser.new.call(params["google-place"])
   end
 
   def create_google_place_variables(google_place_id)
