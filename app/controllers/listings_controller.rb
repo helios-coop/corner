752cbc5839aa654a25483aaf7287f34ade4ae43f
David Ladowitz <david@ladowitz.com>
2018-04-15 14:43:32 -0700

    Fix bug

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -14,7 +14,6 @@ class ListingsController < ApplicationController
   def new
     @listing = Listing.new
     @currencies = Currency.all
-    @states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
   end
 
   def create
