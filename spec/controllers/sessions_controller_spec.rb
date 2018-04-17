8f1c60001bf4b90ef4571fa9500afe57525e7c46
Robert Fletcher <mockdeep@users.noreply.github.com>
2018-04-17 12:52:06 +0800

    Upgrades: update Rails to 5.2 (#148)
    * unlocked Capybara as it works with Rails 5.2
    * introduced the bootsnap gem for faster application boot times, which
      is nice especially for tests.
    * mostly minor configuration updates. The only notable thing is the
      additiona of `active_storage`, Rails' new file storage library.
    * changed `:success` to `:ok` in specs. This was causing a deprecation
      warning, and there is actually no specific `:success` status.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -4,9 +4,9 @@ require "rails_helper"
 
 RSpec.describe SessionsController, type: :controller do
   describe "GET #new" do
-    it "returns http success" do
+    it "returns http ok" do
       get :new
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:ok)
     end
   end
 
