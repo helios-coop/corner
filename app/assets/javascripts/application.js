3cac7fc3196b2d4e2991bf051a7aa85849df17bd
David Ladowitz <david@ladowitz.com>
2018-05-02 16:03:03 -0700

    Initial category search working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,5 @@
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
-//
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
 // vendor/assets/javascripts directory can be referenced here using a relative path.
 //
