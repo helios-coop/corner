4073b65d80de9c0f69559de0c45c7fc42fe628b3
Robert Fletcher <lobatifricha@gmail.com>
2018-05-04 20:43:57 -0700

    Rubocop: pull list of states out into yml config
    A quick approach. I didn't put a ton of thought into where to locate the
    `yml` file, or the method I added to `ApplicationRecord`, but figured
    this was a step in the right direction.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,4 +2,8 @@
 
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.load_data(name)
+    YAML.safe_load(File.read(Rails.root.join("config/data/#{name}.yml"))).freeze
+  end
 end
