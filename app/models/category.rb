1b123dbfe27919778b8c623ccc11c59a4b51022f
Robert Fletcher <mockdeep@users.noreply.github.com>
2018-05-11 01:52:00 +0800

    Seeds: move category creation method to seeds (#194)
    Now we can run `rake db:seed` to load this data. Not a big deal, but
    there's an N+1 query here, so if we get around to integrating a bulk
    import library such as `activerecord-import`, might be worth swapping in
    here as well.
    
    https://github.com/zdennis/activerecord-import

--- app/models/category.rb
+++ app/models/category.rb
@@ -11,12 +11,4 @@ class Category < ApplicationRecord
   pg_search_scope :search_by_name,
                   against: :name,
                   using: { tsearch: { prefix: true } }
-
-  CATEGORY_NAMES = load_data("category_names")
-
-  def self.update_all_categories
-    CATEGORY_NAMES.each do |category_name|
-      Category.find_or_create_by(name: category_name)
-    end
-  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database
 # with its default values. The data can then be loaded with the rails db:seed
 # command (or created alongside the database with db:setup).
@@ -7,3 +8,12 @@
 #
 #  movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #  Character.create(name: 'Luke', movie: movies.first)
+
+def load_data(name)
+  YAML.safe_load(File.read(Rails.root.join("config/data/#{name}.yml"))).freeze
+end
+
+category_names = load_data("category_names")
+(category_names - Category.pluck(:name)).each do |category_name|
+  Category.create!(name: category_name)
+end
