e22a7f43afb75e4af7e12d7bce596b961be0f3be
Robert Fletcher <lobatifricha@gmail.com>
2018-05-17 20:06:47 -0700

    Views: pull icon path into currency model

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -5,4 +5,8 @@ class Currency < ApplicationRecord
 
   has_many :currencies_listings
   has_many :listings, through: :currencies_listings
+
+  def icon_path
+    "crypto_icons/32/color/#{symbol.downcase}.png"
+  end
 end
